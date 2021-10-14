ab78e813ecf32fd9dadccfa768ef65260739cf9a
AbdielLopezChavez <92111640+AbdielLopezChavez@users.noreply.github.com>
2021-10-14 11:21:15 -0500

    Add files via upload
    Version 3. Multiplicación

--- src/Calculadora/Calculadora.java
+++ src/Calculadora/Calculadora.java
@@ -14,6 +14,7 @@ public class Calculadora {
         System.out.println(".: MENU :.");
         System.out.println("Seleccione una opcion:");
         System.out.println("1.-Suma");
+        System.out.println("2.-Resta");
         opc = sc.nextInt();
         
         switch(opc){
@@ -24,11 +25,35 @@ public class Calculadora {
                 num2 = sc.nextInt();
                 suma(num1,num2);
                 break;
+            case 2: 
+                System.out.println("Ingrese el primer numero:");
+                num1 = sc.nextInt();
+                System.out.println("Ingrese el segundo numero:");
+                num2 = sc.nextInt();
+                resta(num1,num2);
+                break;
+            case 3: 
+                System.out.println("Ingrese el primer numero:");
+                num1 = sc.nextInt();
+                System.out.println("Ingrese el segundo numero:");
+                num2 = sc.nextInt();
+                multi(num1,num2);
+                break;
         }
     }
     public static void suma(int num1, int num2){
         int res = num1+num2;
         System.out.println("La suma es: "+res);
+       
+    }
+    public static void resta(int num1, int num2){
+        int res = num1-num2;
+        System.out.println("La resta es: "+res);
+       
+    }
+    public static void multi(int num1, int num2){
+        int multi = num1*num2;
+        System.out.println("La multiplicacion es: "+multi);
        
     }
 }
